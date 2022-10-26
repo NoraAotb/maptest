9bac8e21964129b97e1f024a90bb9589c882fcef
Norah Alotaibi <norah3bass.otb@hotmail.com>
2022-10-26 12:15:48 +0300

    Initial Commit

---
+++ maptest/maptestApp.swift
@@ -0,0 +1,17 @@
+//
+//  maptestApp.swift
+//  maptest
+//
+//  Created by Norah Abass AlOtaibi on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct maptestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
